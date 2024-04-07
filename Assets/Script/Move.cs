97fe30ce017a2903f4fbbfafe6c20da4d59bc2c0
Andrei Huk <145161555+AndreiHukGameDev@users.noreply.github.com>
2024-04-08 00:56:39 +0200

    add forcemode
    add forcemode and max angular velocity

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
@@ -7,19 +5,19 @@ public class Move : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _rotationSpeed;
     
-    private Rigidbody _rb;
-
+    private  Rigidbody _rb;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _rb.maxAngularVelocity = 2.5f;
+        
     }
-
     private void FixedUpdate()
     {
         float sideForce = Input.GetAxis("Horizontal") * _rotationSpeed;
         float forwardForce = Input.GetAxis("Vertical") * _speed;
-
-        _rb.AddRelativeForce(0.0f, 0.0f, forwardForce);
-        _rb.AddRelativeTorque(0.0f, sideForce, 0.0f);
+        
+        _rb.AddRelativeForce(0.0f, 0.0f, forwardForce, ForceMode.VelocityChange);
+        _rb.AddRelativeTorque(0.0f, sideForce, 0.0f, ForceMode.VelocityChange);
     }
 }
